909940b18d676b20d9c3214242fdadbf11f47ba0
garry0325 <34297472+garry0325@users.noreply.github.com>
2017-12-28 21:47:18 +0800

    Add files via upload

---
+++ dettagli.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+SERVICE='omxplayer'
+AUDIOPATH="/home/pi/Desktop/Ornella Vanoni - Dettagli.aac"
+
+while true; do
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+echo "runing" # sleep 1
+else
+omxplayer -o hdmi -vol -1200 $AUDIOPATH
+
+fi
+done
+
